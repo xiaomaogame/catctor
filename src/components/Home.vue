03d45fe3f84ac90d7a50c2708378c14f8afd3596
Administrator <Administrator@MS-KTYEQWUBATIQ>
2024-05-08 13:39:25 +0800

    黑子别跑

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -44,7 +44,7 @@
 
 				</div>
 			</el-col>
-			<el-col :span="12">
+			<el-col :span="12" style="min-width: 755px;">
 				<el-card class="box-card" v-loading="loading" element-loading-text="拼命加载中"
 					element-loading-background="rgba(0, 0, 0, 0.8)">
 					<div class="mainPage">
